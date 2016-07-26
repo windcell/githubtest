374ccc34deeaa62e6bc4c54c4b8e5c353faeb36a
windcell <op_in1000@yahoo.co.jp>
2016-07-26 20:21:38 +0900

    aaaa

---
+++ app/src/main/res/layout/test.cpp
@@ -0,0 +1,5 @@
+//
+// Created by brumaire on 2016/07/26.
+//
+
+#include "test.h"
---
+++ app/src/main/res/layout/test.h
@@ -0,0 +1,16 @@
+//
+// Created by brumaire on 2016/07/26.
+//
+
+#ifndef GITHUBTEST_TEST_H
+#define GITHUBTEST_TEST_H
+
+
+
+class test {
+
+};
+
+
+
+#endif //GITHUBTEST_TEST_H
